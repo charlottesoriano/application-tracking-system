e986f2d30225325112cc912fb8988efc2b5d5b10
charlotte <charlotte@evotechdc.com>
2026-08-25 14:57:05 +0800

    added log out button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { usePuterStore } from '~/lib/puter'
 
 const Navbar = () => {
+    const { auth } = usePuterStore()
+
     return (
         <nav className='navbar'>
             <Link to='/'>
                 <p className='text-2xl font-bold text-gradiewnt'>RESUMIND</p>
             </Link>
-            <Link to='/upload' className='primary-button w-fit'>
-                Upload Resume
-            </Link>
+            <div className='flex flex-row items-center gap-4'>
+                <Link to='/upload' className='primary-button w-fit'>
+                    Upload Resume
+                </Link>
+                {auth.isAuthenticated && (
+                    <button onClick={auth.signOut} className='primary-button w-fit'>
+                        Log Out
+                    </button>
+                )}
+            </div>
         </nav>
     )
 }
